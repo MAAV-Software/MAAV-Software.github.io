1fa4369611764de712d6940f8cc0f5ed0828d6e3
Dervin Tian <dervint@umich.edu>
2025-11-17 18:59:31 -0500

    Updated Software Team Page

--- _app/immutable/nodes/7.DSx0zVDV.js
+++ _app/immutable/nodes/7.DSx0zVDV.js
@@ -1,8 +1,21 @@
-import{h as r,t as l,a as m}from"../chunks/4VM-x7rA.js";import"../chunks/B9fgz59Y.js";import{$ as d,Z as h,V as p,_ as u}from"../chunks/DoBdwIHE.js";import{b as o}from"../chunks/CEpIJIzL.js";import{C as c}from"../chunks/DQyDsOwF.js";import{b as f}from"../chunks/DMwpYjxI.js";const g=""+new URL("../assets/Git.DOVXCQjC.png",import.meta.url).href;var b=l(`<a class="font-light text-xl hover:text-blue-600 active:text-blue-800">Back to Subteams</a> <img alt="Test Drone" class="w-11/12 h-auto" width="3888" height="2592"> <h3 class="text-xl font-medium">Computer Vision</h3> <p>Our Software team leverages the Raspberry Pi Arducam to capture and analyze the surrounding environment. 
-    To meet the challenges of our new mission, we develop and refine state-of-the-art algorithms for tasks such as detection, mapping, and tracking using images from the drone. 
+import{h as r,t as l,a as m}from"../chunks/4VM-x7rA.js";import"../chunks/B9fgz59Y.js";import{$ as d,Z as h,V as p,_ as u}from"../chunks/DoBdwIHE.js";import{b as o}from"../chunks/CEpIJIzL.js";import{C as c}from"../chunks/DQyDsOwF.js";import{b as f}from"../chunks/DMwpYjxI.js";const g=""+new URL("../assets/software.jpeg",import.meta.url).href;var b=l(`<a class="font-light text-xl hover:text-blue-600 active:text-blue-800">Back to Subteams</a> <img alt="Test Drone" class="w-11/12 h-auto" width="3888" height="2592"> <h3 class="text-xl font-medium">Computer Vision</h3> <p>Our Software team leverages the Raspberry Pi Arducam to capture and analyze the surrounding environment. 
+    To meet the challenges of our new mission, we develop and refine state-of-the-art algorithms for tasks such as detection, autonomous flight, and tracking using images from the drone. 
     These algorithms are executed on a Jetson Orin Nano embedded GPU to ensure high-performance processing in real-time.</p> <h3 class="text-xl font-medium">Guidance, Navigation, and Control</h3> <p>Guidance, Navigation, and Control is a critical focus of our team. 
     We process inputs from the PX4 flight controller to determine the drone's position and generate precise flight paths to guide it to its destination.
     The Software subteam also develops a series of control states tailored to different mission phases, 
-    including mapping, waypoint navigation, landing/takeoff, and object detection/drop operations.</p> <h3 class="text-xl font-medium">Utilities</h3> <p>In addition to core mission tasks, our Software subteam develops tools to support and streamline the team's workflow. 
+    including waypoint navigation, landing/takeoff, and object detection/drop operations.</p> <h3 class="text-xl font-medium">Utilities</h3> <p>In addition to core mission tasks, our Software subteam develops tools to support and streamline the team's workflow. 
     We maintain a Gazebo simulation that replicates the competition environment, simulating sensor inputs to test and validate our software in a realistic virtual setting. 
-    This simulated world includes key mission elements such as airfields, waypoints, and drop targets, ensuring our software is mission-ready.</p> <h3 class="text-xl font-medium">Team Members</h3> <ol class="list-disc flex justify-center items-center flex-wrap gap-x-6 font-light line-height-1"><li>Dermot Molony</li> <li>Dervin Tian</li> <li>Habib Shakour</li> <li>Jacob Eggerd</li> <li>Lindsay Huynh</li> <li>Mehmed Kerem Uludag</li></ol>`,1);function S(s){const e="Software";r(t=>{d.title=e}),c(s,{title:e,children:(t,v)=>{var i=b(),a=h(i);o(a,"href",`${f}/subteams/`);var n=p(a,2);o(n,"src",g),u(16),m(t,i)},$$slots:{default:!0}})}export{S as component};
+    This simulated world includes key mission elements such as airfields, waypoints, and drop targets, ensuring our software is mission-ready.</p> <h3 class="text-xl font-medium">Team Members</h3> <ol class="list-disc flex justify-center items-center flex-wrap gap-x-6 font-light line-height-1">
+    <li>  Dervin Tian  </li>
+    <li>  Habib Shakour  </li>
+    <li>  Jerry Zhu  </li>
+    <li>  Lindsay Huynh  </li>
+    <li>  Steffano Martinez </li>
+    <li>  Oscar de Castro </li>
+    <li>  Mimmo Baluyut </li>
+    <li>  Noah Horowitz </li>
+    <li>  Charles Ferguson </li>
+    <li>  Quinn Hague </li>
+    <li>  Swati Sahu </li>
+    </ol>`,1);
+    function S(s){const e="Software";r(t=>{d.title=e}),c(s,{title:e,children:(t,v)=>{var i=b(),a=h(i);o(a,"href",`${f}/subteams/`);var n=p(a,2);o(n,"src",g),u(16),m(t,i)},$$slots:{default:!0}})}export{S as component};
